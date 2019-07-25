d617d5dd9d4d0d4e9893548dee57c1f6f45b1298
Jaeyoon Song <jyo3on@gmail.com>
2019-07-25 12:33:27 +0900

    create index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,40 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', function() {
+    var contribution = document.querySelector(".progress-bar");
+    var elem = document.querySelector('.collapsible');
+    var isDones = document.querySelectorAll(".isDone");
+    
+    // init collapsible
+    M.Collapsible.init(elem);
+    var instance = M.Collapsible.getInstance(elem);
+    
+    instance.open(1);
+
+    // task 1
+    var status = false;
+    document.querySelector("#start1").onclick = function(e) {
+        if (!status) {
+            this.innerText = "click to finish!";
+            status = true;
+        }
+        else {
+            instance.open(1);
+            status = false;
+            contribution.style.width = "10%";
+            isDones[0].style.opacity = 1;
+        }
+        
+    }
+
+    document.querySelector("#start2").onclick = function(e) {
+        if (!status) {
+            document.querySelector(".next").classList.add("shown");
+            status = true;
+        }
+        
+    }
+
+});
