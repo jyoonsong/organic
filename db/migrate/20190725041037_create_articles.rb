31feb5e5812687b2768250334dde82215faaa274
Jaeyoon Song <jyo3on@gmail.com>
2019-07-29 16:32:19 +0900

    add columns to article table

--- db/migrate/20190725041037_create_articles.rb
+++ db/migrate/20190725041037_create_articles.rb
@@ -1,6 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      t.string :topic
+      t.string :subtopic
+      t.string :title
+      t.text :metadata
+      t.text :content
+      t.integer :expert_score
 
       t.timestamps
     end
